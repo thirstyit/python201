a2baa6cdba50812b8e5aeea3289d745934c70d47
martin-martin <breuss.martin@gmail.com>
2021-04-03 16:49:24 +0200

    Add listcomp labs

---
+++ 201_01_more-datatypes/01_19_comprehension_syntax.py
@@ -0,0 +1,5 @@
+# Use a list comprehension to create a list that contains the individual
+# letters of the word "codingnomads":
+# ['c', 'o', 'd', 'i', 'n', 'g', 'n', 'o', 'm', 'a', 'd', 's']
+
+word = "codingnomads"
