e87b0338c2ec7a2091675d7ec96a4ea0f1c241a9
Martin Breuss <martin-martin@users.noreply.github.com>
2021-12-13 09:27:57 +0100

    Update lab instructions
    Co-authored-by: giladgressel <giladgressel@gmail.com>

--- 06_advanced-python-concepts/06_08_based_dictcomp.py
+++ 06_advanced-python-concepts/06_08_based_dictcomp.py
@@ -1,17 +1,14 @@
-# Using the given variables `base` and `digits`, write a dictionary comprehension
-# that maps each integer between `0` and `999` to the list of three digits 
-# that represents that integer in base 10. That is, the value should be:
+# Write a dictionary comprehension that maps each integer
+# between `0` and `999` to a list of the digits that represents
+# that integer in base 10. That is, your output should be:
 #
-# {0: [0, 0, 0], 1: [0, 0, 1], 2: [0, 0, 2], 3: [0, 0, 3], ...,
-# 10: [0, 1, 0], 11: [0, 1, 1], 12: [0, 1, 2], ...,
+# {0: [0], 1: [1], 2: [2], 3: [3], ...,
+# 10: [1, 0], 11: [1, 1], 12: [1, 2], ...,
 # 999: [9, 9, 9]}
 #
-# Your expression should work for any base. For example, 
-# if you instead assign 2 to base and assign {0,1} to digits, 
-# the value should be:
+# CHALLENGE: Write another dictionary comprehension that works the same
+# but for base 2 (binary)! The output values should be:
 #
-# {0: [0, 0, 0], 1: [0, 0, 1], 2: [0, 1, 0], 3: [0, 1, 1],
-# ..., 7: [1, 1, 1]}
-
-base = 10
-digits = set(range(base))
+# {0: [0, 0, 0], 1: [0, 0, 1], 2: [0, 1, 0], 3: [0, 1, 1], ...,
+# 7: [1, 1, 1], 8: [1, 0, 0, 0], 9: [1, 0, 0, 1], ...,
+# 999: [1, 1, 1, 1, 1, 0, 0, 1, 1, 1]}
