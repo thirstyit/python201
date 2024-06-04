f92c98d47ac00f1e0ab2846fda55091328e774d0
Sam Ferguson <sam@thirstyit.com.au>
2024-06-05 09:40:17 +1000

    Solved power_dict

--- 02_more-datatypes/02_13_power_dict.py
+++ 02_more-datatypes/02_13_power_dict.py
@@ -1,3 +1,15 @@
 # Write a script that creates a dictionary of keys, `n`
 # and values `n * n` for numbers 1 to 10. For example:
 # result = {1: 1, 2: 4, 3: 9, ... and so on}
+
+
+dict_ = {}
+
+key_ = 1
+
+while key_ <= 10:
+    value = key_ * key_
+    dict_[key_] = value
+    key_+=1
+
+print(dict_)
