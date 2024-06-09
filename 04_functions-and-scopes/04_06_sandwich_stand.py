f0c1836d173cba2c60c7135c2ca14fcbb643ee81
Sam Ferguson <sam@thirstyit.com.au>
2024-06-09 21:00:31 +1000

    completed sandswich exercise

--- 04_functions-and-scopes/04_06_sandwich_stand.py
+++ 04_functions-and-scopes/04_06_sandwich_stand.py
@@ -3,3 +3,14 @@
 # - takes an arbitrary amount of toppings
 # - returns a string representing a sandwich with the bread on top
 #   and bottom, and the toppings in between.
+
+
+def make_sandwich(bread, *args):
+    print(bread)
+    for t in args:
+        print(t)
+        
+    print(bread)
+
+
+make_sandwich("Wholemeal", "Ham", "Cheese", "Sauce")
