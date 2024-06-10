ff96fd8a796ae308ca0b31e900dd025dba67e733
Sam Ferguson <sam@thirstyit.com.au>
2024-06-10 18:54:34 +1000

    divisibility exercise complete

--- 04_functions-and-scopes/04_07_determine_divisible.py
+++ 04_functions-and-scopes/04_07_determine_divisible.py
@@ -7,3 +7,43 @@
 # - call your functions, passing in the user input as the arguments,
 #   and set their output equal to new variables 
 # - print your the result variables with descriptive messages
+
+
+
+
+def divisibleByFourOrSeven(num):
+    
+    
+    """
+    Checks if a number is divisible by four or seven and returns a bool
+    """
+    divisible = False
+    
+    if num % 7 == 0 or num % 4 == 0:
+        divisible = True
+        
+    return divisible
+
+
+def divisibleByFourAndSeven(num):
+    
+    
+    """
+    Checks if a number is divisible by four and seven and returns a bool
+    """
+    divisible = False
+    
+    if num % 7 == 0 and num % 4 == 0:
+        divisible = True
+    
+    
+    return divisible
+
+big_num = input("Enter a number between one and a billion: ")
+
+big_num = int(big_num)
+
+seven_or_four = divisibleByFourOrSeven(big_num)
+seven_and_four = divisibleByFourAndSeven(big_num)
+
+print(f"That {str(big_num)} is divisible by four or seven is {seven_or_four}. \n That {str(big_num)} is divisible by four and seven is {seven_and_four}. \n")
